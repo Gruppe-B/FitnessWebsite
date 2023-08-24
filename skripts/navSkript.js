28a72caa596081e828937d052f69c934e819a151
Erdabi <135817985+Erdabi@users.noreply.github.com>
2023-08-24 13:24:36 +0000

    Name changes and formatet

--- skripts/navSkript.js
+++ skripts/navSkript.js
@@ -1,18 +1,19 @@
+// function for the dropdown nav
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-    function scrollToTarget() {
-      const targetSection = document.querySelector('#myTopnav');
-      
-      // Scroll to the target section using scrollIntoView
-      targetSection.scrollIntoView({
-      });
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-  
+  function scrollToTarget() {
+    const targetSection = document.querySelector('#myTopnav');
+
+    // Scroll to the target section using scrollIntoView
+    targetSection.scrollIntoView({
+    });
+  }
+
   // Call the scrollToTarget function when needed
   scrollToTarget();
-  }
+}
